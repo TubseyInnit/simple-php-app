b3c1bfcaceb6f6f2c21dbd12efec4ee2cc18bf84
Tobey <tobesywobesy@gmail.com>
2023-10-08 21:31:47 +0100

    Update home.php

--- htdocs/simple/home.php
+++ htdocs/simple/home.php
@@ -17,21 +17,21 @@ session_start();
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
+        <script type="text/javascript">
+            function logout() {
+                document.cookie = "loggedIn=false;";
+                window.location.replace("index.php");
+            }
+        </script>
     </head>
     <body>
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
             <div class="container px-4 px-lg-5">
                 <a class="navbar-brand" href="home.php">Fansite</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
+                <button class="navbar-toggler" type="button" onclick="logout()">
+                    Logout
                 </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="home.php">Logout</a></li>
-                    </ul>
-                </div>
             </div>
         </nav>
         <!-- Page Header-->
